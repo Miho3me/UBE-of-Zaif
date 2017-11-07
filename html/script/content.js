3f0ae79bac8098bf67e5febded73307c3f51f843
masaya <masaya@kurastagram.local>
2017-11-07 13:58:42 +0900

    通知が消えない問題を修正

--- html/script/content.js
+++ html/script/content.js
@@ -1,8 +1,8 @@
-let count,developer_id,developer_last,userid,username,message,procesing,click
+let count,developer_last,userid,username,message,procesing,click
 
 count = 1
 processing = "off"
-developer_id = "3be265a8a31ae64ccca66339b284f45b8e6e9300"
+const developer_id = "3be265a8a31ae64ccca66339b284f45b8e6e9300"
 
 $(function(){
   $("#cc_area").on("DOMNodeInserted",function(){
@@ -29,13 +29,15 @@ $(function(){
   }),
   $(document).on("click","button",function(){
     click = $(this).attr("class")
+    console.log(click)
     if(click == "ng-button"){
       userid = $(this).parent().find("span").attr("title")
       username = $(this).parent().find("span").text()
       NG_INPUT(username,userid)
-    }else if(click == "Notification-Ex"){
+    }else if(click == "close notifi-hide"){
       //ヘッダーの通知
-      $.cookie('alert_1_2_1', 'closed', { path: '/', expires: 1800 });
+      console.log("hide")
+      $.cookie('alert_1_2_4', 'closed', { path: '/', expires: 1800 });
     }
   })
 })
